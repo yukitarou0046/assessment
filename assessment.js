4bd73109691773bc2f75f80004263b673b77b530
yukitarou0046 <3.yuki.26.yuki@gmail.com>
2025-04-14 18:02:17 +0100

    Bootstrapの使用による変更

--- assessment.js
+++ assessment.js
@@ -17,14 +17,28 @@ assessmentButton.addEventListener(
 
     // 診断結果表示エリアの作成
     resultDivision.innerText = '';
-    const heading = document.createElement('h3');
-    heading.innerText = '診断結果';
-    resultDivision.appendChild(heading);
+
+    //headerDivision の作成
+    const headerDivision = document.createElement('div');
+    headerDivision.setAttribute('class','card-header text-bg-primary');
+    headerDivision.innerText = '診断結果';
+
+    //bodyDivision の作成
+    const bodyDivision = document.createElement('div');
+    bodyDivision.setAttribute('class','card-body');
 
     const paragraph = document.createElement('p');
+    paragraph.setAttribute('class','card-text');
     const result = assessment(userName);
     paragraph.innerText = result;
-    resultDivision.appendChild(paragraph);
+    bodyDivision.appendChild(paragraph);
+
+    //resultDivision に BootStrapのスタイルを適用する
+    resultDivision.setAttribute('class','card');
+
+    //headerDivisionとbodyDibisionをresultDibisionに差し込む
+    resultDivision.appendChild(headerDivision);
+    resultDivision.appendChild(bodyDivision);
 
     // ツイートエリアの作成
     tweetDivision.innerText = '';
